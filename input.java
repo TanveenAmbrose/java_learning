0554640bb034870e7d63e5e4f9f38da8d9a61632
Tanveen Ambrose <128674440+TanveenAmbrose@users.noreply.github.com>
2024-02-29 18:34:44 +0600

    input all

--- input.java
+++ input.java
@@ -3,8 +3,15 @@ import java.util.*;
 public class input {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
+        String input = sc.nextLine();
         System.out.println(input);
-
+        int number = sc.nextInt();
+        System.out.println(number);
+        float number2 = sc.nextFloat();
+        System.out.println(number2);
+        Byte number3 = sc.nextByte();
+        System.out.println(number3);
+        double number4 = sc.nextDouble();
+        System.out.println(number4);
     }
 }
