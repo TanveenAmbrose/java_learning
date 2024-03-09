859dd89d523d9befea084acf7b9e8f96112a9822
Tanveen Ambrose <128674440+TanveenAmbrose@users.noreply.github.com>
2024-03-09 19:46:31 +0600

    for loop

---
+++ forloop.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class forloop {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int n = sc.nextInt();
+        for(i = 1; i <= n; i++){
+            System.out.println("Hello I am Tanveen");
+        }
+
+    }
+    
+}
