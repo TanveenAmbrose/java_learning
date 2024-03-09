bb6fac28e247faed4bd5d255960b5ea3829538fd
Tanveen Ambrose <128674440+TanveenAmbrose@users.noreply.github.com>
2024-03-09 20:09:13 +0600

    do-while loop

---
+++ dowhileloop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class dowhileloop {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        do{
+            System.out.println("Hi I am Pro");
+            n++;
+        }
+        while(n <= 10);
+           
+    }
+    
+}
